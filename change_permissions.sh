358d11af918569c826871208ac387c89a449dce3
Nikhil Bhagunde <nikhilbhagunde@benchmarkitsolutions.com>
2019-06-18 15:04:02 +0530

    working permission1

--- change_permissions.sh
+++ change_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R ec2-user:apache /var/www/
+chown -R ac2-user:apache /var/www/
 chmod 2775 /var/www
 #find /var/www -type d -exec sudo chmod 2775 {} \;
 #find /var/www -type f -exec sudo chmod 0664 {} \;
