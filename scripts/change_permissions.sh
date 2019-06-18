d5e71c93aa3b668706f447a83ecd838abf1b4f39
Nikhil Bhagunde <nikhilbhagunde@benchmarkitsolutions.com>
2019-06-18 11:24:49 +0530

    add permision script

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chown -R ac2-user:apache /var/www/
+chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+chmod -R 777 /var/www/html/advuser/frontend/web/assets/
+chmod -R 777 /var/www/html/advuser/frontend/runtime/
+chmod -R 777 /var/www/html/advuser/backend/web/assets/
+chmod -R 777 /var/www/html/advuser/backend/runtime/
+chmod -R 777 /var/www/html/advuser/api/web/assets/
+chmod -R 777 /var/www/html/advuser/api/runtime/
