548ef325cec3c86f0e0d90c4002ac66c8cce6466
Nikhil Bhagunde <nikhilbhagunde@benchmarkitsolutions.com>
2019-06-18 13:42:50 +0530

    working permission3

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chown -R ac2-user:apache /var/www/
 chmod 2775 /var/www
- 
 #find /var/www -type d -exec sudo chmod 2775 {} \;
 #find /var/www -type f -exec sudo chmod 0664 {} \;
 #chmod -R 777 /var/www/html/advuser/frontend/web/assets/
